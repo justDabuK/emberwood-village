5c2e83bc7abcba967d7534a9a8463b9f66bb7a05
justDaBuK <kevinjust87@gmail.com>
2024-03-29 23:28:36 +0100

    feat: group spells by action

--- src/components/character/SpellCheatSheet.vue
+++ src/components/character/SpellCheatSheet.vue
@@ -20,6 +20,25 @@ const concentration = defineModel<boolean>("concentration");
 
 const knownSpellList = props.allSpells.filter((spell) => props.knownSpellNameList.includes(spell.frontmatter.title));
 
+const isBonusActionSpell = (spell: MarkdownInstance<Record<string, any>>) => spell.frontmatter.castingTime.includes('bonus');
+const isActionSpell = (spell: MarkdownInstance<Record<string, any>>) => spell.frontmatter.castingTime.includes('action') && !isBonusActionSpell(spell);
+const isReactionSpell = (spell: MarkdownInstance<Record<string, any>>) => spell.frontmatter.castingTime.includes('reaction');
+const isRitualSpell = (spell: MarkdownInstance<Record<string, any>>) => spell.frontmatter.ritual;
+
+const bonusActionSpellList = knownSpellList.filter(isBonusActionSpell);
+const actionSpellList = knownSpellList.filter(isActionSpell);
+const reactionSpellList = knownSpellList.filter(isReactionSpell);
+const ritualSpellList = knownSpellList.filter(isRitualSpell);
+const remainingSpellList = knownSpellList.filter((spell) => !isBonusActionSpell(spell) && !isActionSpell(spell) && !isReactionSpell(spell) && !isRitualSpell(spell));
+
+const spellSectionList = [
+  {title: 'Action', spells: actionSpellList},
+  {title: 'Bonus Action', spells: bonusActionSpellList},
+  {title: 'Reaction', spells: reactionSpellList},
+  {title: 'Ritual', spells: ritualSpellList},
+  {title: 'Remaining', spells: remainingSpellList}
+];
+
 const getCostlyComponent = (components: string) => {
   const match = components.match(/\((.+)\)/);
   return match ? match[1] : "";
@@ -56,14 +75,19 @@ const isCostlySpell = (components: string) => {
       </div>
     </div>
     <div class="spell-cheat-sheet-body">
-      <a v-for="spell in knownSpellList" :key="spell.frontmatter.title" :href="spell.url" :class="['card', 'spell', spell.frontmatter.duration.includes('Concentration') ? 'concentration' : '']">
-        <SpellSchoolIcon class="spell-school-icon" :spell-school="spell.frontmatter.school"/>
-        <SpellRangeIcon class="spell-range-icon" :range="spell.frontmatter.range"/>
-        <p>{{ spell.frontmatter.title }}</p>
-        <p v-if="isCostlySpell(spell.frontmatter.components)">
-          {{ getCostlyComponent(spell.frontmatter.components) }}
-        </p>
-      </a>
+      <div v-for="spellSection in spellSectionList" :key="spellSection.title" class="spell-section">
+        <h2>{{spellSection.title}}</h2>
+        <div class="spell-section-spell-list">
+          <a v-for="spell in spellSection.spells" :key="spell.frontmatter.title" :href="spell.url" :class="['card', 'spell', spell.frontmatter.duration.includes('Concentration') ? 'concentration' : '']">
+            <SpellSchoolIcon class="spell-school-icon" :spell-school="spell.frontmatter.school"/>
+            <SpellRangeIcon class="spell-range-icon" :range="spell.frontmatter.range"/>
+            <p>{{ spell.frontmatter.title }}</p>
+            <p v-if="isCostlySpell(spell.frontmatter.components)">
+              {{ getCostlyComponent(spell.frontmatter.components) }}
+            </p>
+          </a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -82,33 +106,45 @@ const isCostlySpell = (components: string) => {
   }
 
   .spell-cheat-sheet-body {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    display: flex;
+    flex-direction: column;
     gap: 20px;
 
-    .spell {
-      position: relative;
+    .spell-section {
       display: flex;
       flex-direction: column;
-      text-align: center;
-      color: var(--text-color);
+      gap: 20px;
 
-      .spell-school-icon {
-        position: absolute;
-        top: 10px;
-        left: 10px;
-        height: 30px;
-        width: 30px;
-        color: var(--text-color-darker-1);
-      }
+      .spell-section-spell-list {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+        gap: 20px;
 
-      .spell-range-icon {
-        position: absolute;
-        top: 10px;
-        right: 10px;
-        height: 30px;
-        width: 30px;
-        color: var(--text-color-darker-1);
+        .spell {
+          position: relative;
+          display: flex;
+          flex-direction: column;
+          text-align: center;
+          color: var(--text-color);
+
+          .spell-school-icon {
+            position: absolute;
+            top: 10px;
+            left: 10px;
+            height: 30px;
+            width: 30px;
+            color: var(--text-color-darker-1);
+          }
+
+          .spell-range-icon {
+            position: absolute;
+            top: 10px;
+            right: 10px;
+            height: 30px;
+            width: 30px;
+            color: var(--text-color-darker-1);
+          }
+        }
       }
     }
   }
