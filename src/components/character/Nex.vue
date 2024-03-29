8795835f3395fc2f2acdf9b296f4d926ef522b35
justDaBuK <kevinjust87@gmail.com>
2024-03-29 12:21:09 +0100

    feat: add utilities for Nex stats

--- src/components/character/Nex.vue
+++ src/components/character/Nex.vue
@@ -1,17 +1,125 @@
 <script setup lang="ts">
-import {type Creature, TypeOfRest} from "../../scripts/cheatSheetTypes.ts";
+import {type AbilityScores, type Creature, getModifier, Skill, TypeOfRest} from "../../scripts/cheatSheetTypes.ts";
 import RoundCheatSheet from "./RoundCheatSheet.vue";
 import {useStorage} from "@vueuse/core";
+import type {MarkdownInstance} from "astro";
+import SkillCheatSheet from "./SkillCheatSheet.vue";
 
-const NEX_LEVEL = 3;
-const NEX_PROFICIENCY_BONUS = 2;
+defineProps<{
+  allSpells: MarkdownInstance<Record<string, any>>[];
+}>();
+
+const NEX_LEVEL = 4;
+
+const getHideArmorClass = (dexModifier: number) => {
+  return 12 + Math.min(dexModifier, 2);
+}
+
+const getChainShirtArmorClass = (dexModifier: number) => {
+  return 13 + Math.min(dexModifier, 2);
+}
+
+const getProficiencyBonus = (level: number) => {
+  if(level < 5) {
+    return 2;
+  } else if(level < 9) {
+    return 3;
+  } else if(level < 13) {
+    return 4;
+  } else if(level < 17) {
+    return 5;
+  } else {
+    return 6;
+  }
+};
+
+const getCantripsKnown = (level: number) => {
+  if(level < 4) {
+    return 3;
+  } else if(level < 10) {
+    return 4;
+  } else {
+    return 5;
+  }
+};
+
+const getSpellSlots = (level: number) => {
+  if(level === 1) {
+    return 1;
+  } else if (level < 5) {
+    return 2;
+  } else if(level < 9){
+    return 3;
+  } else if(level < 13) {
+    return 4;
+  } else if(level < 17) {
+    return 5;
+  } else {
+    return 6;
+  }
+}
+
+const getSlotLevel = (level: number) => {
+  if(level < 3) {
+    return 1;
+  } else if(level < 5) {
+    return 2;
+  } else if(level < 7) {
+    return 3;
+  } else if(level < 9) {
+    return 4;
+  } else  {
+    return 5;
+  }
+}
+
+const getTheoriesKnown = (level: number) => {
+  if(level === 1) {
+    return 0;
+  } else {
+    return Math.floor(level/ 2) + 1;
+  }
+};
+
+const ABILITY_SCORES: AbilityScores = {
+  STR: 8,
+  DEX: 14,
+  CON: 16,
+  INT: 20,
+  WIS: 10,
+  CHA: 12
+};
+
+const MODIFIER: AbilityScores = {
+  STR: getModifier(ABILITY_SCORES.STR),
+  DEX: getModifier(ABILITY_SCORES.DEX),
+  CON: getModifier(ABILITY_SCORES.CON),
+  INT: getModifier(ABILITY_SCORES.INT),
+  WIS: getModifier(ABILITY_SCORES.WIS),
+  CHA: getModifier(ABILITY_SCORES.CHA)
+};
+
+const SAVING_THROW_PROFICIENCIES_LIST: (keyof AbilityScores)[]= [
+  "STR",
+  "CON"
+];
+
+const SKILL_PROFICIENCIES = [
+  Skill.Athletics,
+  Skill.Survival,
+  Skill.Persuasion
+]
+
+const SKILL_EXPERTIES = [
+  Skill.Intimidation
+]
 
 const defaultNexCreatureList = [
   {
     name: "Nex",
     hitPoints: {
-      current: (5 + 3) * NEX_LEVEL,
-      max: (5 + 3) * NEX_LEVEL,
+      current: (5 + MODIFIER.CON) * NEX_LEVEL,
+      max: (5 + MODIFIER.CON) * NEX_LEVEL,
       temporary: 0,
       hitDice: {
         flags: [...Array(NEX_LEVEL)].fill(false),
@@ -21,7 +129,7 @@ const defaultNexCreatureList = [
     },
     contamination: 0,
     exhaustion: 0,
-    armorClass: 14,
+    armorClass: getHideArmorClass(MODIFIER.DEX),
     sectionList: [
       {
         title: 'Action',
@@ -29,11 +137,11 @@ const defaultNexCreatureList = [
         subsections: [
           {
             title: 'Weapon Attack',
-            dice: 'd20+3',
+            dice: `d20+${MODIFIER.DEX + getProficiencyBonus(NEX_LEVEL)}`,
             items: [
-              {name: 'Crossbow light', dice: '1d8+1'},
-              {name: 'Crossbow Hand', dice: '1d6+1'},
-              {name: 'Dagger', dice: '1d4+1'}
+              {name: 'Crossbow light', dice: `1d8+${MODIFIER.DEX}`},
+              {name: 'Crossbow Hand', dice: `1d6+${MODIFIER.DEX}`},
+              {name: 'Dagger', dice: `1d4+${MODIFIER.DEX}`},
             ]
           },
           {
@@ -46,13 +154,6 @@ const defaultNexCreatureList = [
         title: 'Bonus Action',
         used: false,
         subsections: [
-          {
-            title: 'Off-hand attack',
-            dice: 'd20+3',
-            items: [
-              {name: 'Dagger', dice: '1d4'}
-            ]
-          },
           {
             title: 'Command Bernhard',
             description: 'Bernhard needs a command, otherwise he will take the Dodge action'
@@ -100,7 +201,7 @@ const defaultNexCreatureList = [
               flags: [false],
               typeOfRest: TypeOfRest.LONG,
             },
-            description: `${NEX_LEVEL} HP for ${NEX_PROFICIENCY_BONUS} creatures during short rest, 1 per long rest`
+            description: `${NEX_LEVEL} HP for ${getProficiencyBonus(NEX_LEVEL)} creatures during short rest, 1 per long rest`
           }
         ]
       }
@@ -109,12 +210,12 @@ const defaultNexCreatureList = [
   {
     name: 'Bernhard',
     hitPoints: {
-      current: 5 +5 * NEX_LEVEL,
-      max: 5 +5 * NEX_LEVEL,
+      current: 5 + 5 * NEX_LEVEL,
+      max: 5 + 5 * NEX_LEVEL,
     },
     contamination: 0,
     exhaustion: 0,
-    armorClass: 14 + NEX_PROFICIENCY_BONUS,
+    armorClass: 14 + getProficiencyBonus(NEX_LEVEL),
     sectionList: [
       {
         title: 'Action',
@@ -122,7 +223,7 @@ const defaultNexCreatureList = [
         subsections: [
           {
             title: 'Weapon Attack',
-            dice: `d20+${4 + NEX_PROFICIENCY_BONUS}`,
+            dice: `d20+${4 + getProficiencyBonus(NEX_LEVEL)}`,
             items: [
               {name: 'Slam', dice: '1d8+4'},
             ]
@@ -229,11 +330,21 @@ const defaultNexCreatureList = [
 ];
 
 const creatureList = useStorage<Creature[]>('nex-creature-list', defaultNexCreatureList);
-
+const resetToDefault = () => {
+  creatureList.value = defaultNexCreatureList;
+};
 </script>
 
 <template>
-  <RoundCheatSheet v-model="creatureList" class="cheat-sheet" />
+  <RoundCheatSheet v-model="creatureList" @reset-to-default="resetToDefault" class="cheat-sheet" />
+  <SkillCheatSheet
+      :modifiers="MODIFIER"
+      :ability-scores="ABILITY_SCORES"
+      :saving-throw-proficiency-list="SAVING_THROW_PROFICIENCIES_LIST"
+      :skill-proficiency-list="SKILL_PROFICIENCIES"
+      :skill-expertise-list="SKILL_EXPERTIES"
+      :proficiency-bonus="getProficiencyBonus(NEX_LEVEL)"
+  />
 </template>
 
 <style scoped>
