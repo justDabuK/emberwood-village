fb79616d844661a97deb30cbd0fc7d6fe36b46ab
Kevin Just <kevin.just@agile-robots.com>
2024-08-18 17:05:19 +0200

    feat: add giant octopus to eledaina

--- src/components/character/Eledaina.vue
+++ src/components/character/Eledaina.vue
@@ -131,7 +131,7 @@ const giantSpiderSavingThrowProficiencyList: (keyof AbilityScores)[] = [];
 const giantSpiderSkillProficiencies: Skill[] = [Skill.Stealth];
 const giantSpiderProficiencyBonus = 2;
 
-// --- giant spider ---
+// --- giant toad ---
 const giantToadAbilityScores: AbilityScores = {
   STR: 15,
   DEX: 13,
@@ -231,6 +231,114 @@ const giantToad: Creature = {
         {
           title: "Darkvision",
           description: "30 ft. (6 squares)",
+        },
+      ],
+    },
+  ],
+};
+
+// --- giant Octopus ---
+const giantOctopusAbilityScores: AbilityScores = {
+  STR: 17,
+  DEX: 13,
+  CON: 13,
+  INT: 4,
+  WIS: 10,
+  CHA: 4,
+};
+const giantOctopusProficiencyBonus = 2;
+
+const giantOctopus: Creature = {
+  name: "Giant Octopus",
+  hitPoints: {
+    current: 52,
+    max: 52,
+  },
+  contamination: 0,
+  exhaustion: 0,
+  armorClass: 11,
+  abilityScores: giantOctopusAbilityScores,
+  savingThrowProficiencyList: [],
+  skill: {
+    proficiencies: [Skill.Perception, Skill.Stealth],
+    expertise: [],
+  },
+  proficiencyBonus: giantOctopusProficiencyBonus,
+  sectionList: [
+    {
+      title: "Action",
+      used: false,
+      subsections: [
+        {
+          title: "Tentacles ",
+          dice: "d20+5",
+          description: "target is grappled & restrained (escape DC 16)",
+          items: [{ name: "Tentacle (15ft.)", dice: "2d6+3" }],
+        },
+        {
+          title: "Ink Cloud",
+          usages: {
+            flags: [false],
+            typeOfRest: TypeOfRest.SHORT,
+          },
+          description:
+            "if underwater, 20 ft. cloud. Area is heavily obscured for 1 minute",
+        },
+      ],
+    },
+    {
+      title: "Bonus Action",
+      used: false,
+      subsections: [
+        {
+          title: "Combat wild shape",
+          description: `Use spell slots for healing`,
+          dice: "1d8*Lvl",
+        },
+        {
+          title: "Dash",
+          description: `If Ink Cloud as action`,
+        },
+      ],
+    },
+    {
+      title: "Reaction",
+      used: false,
+      subsections: [
+        {
+          title: "Opportunity Attack",
+          description: "See Attack",
+        },
+      ],
+    },
+    {
+      title: "Movement",
+      used: false,
+      subsections: [
+        {
+          title: "Walking 10ft.",
+          description: "(2 squares)",
+        },
+        {
+          title: "Swimming 60ft.",
+          description: "(10 squares)",
+        },
+      ],
+    },
+    {
+      title: "Features",
+      subsections: [
+        {
+          title: "Water Breathing",
+          description: "can only breath under water",
+        },
+        {
+          title: "Hold Breath",
+          description: "can hold breath out of water for 1 hour",
+        },
+        {
+          title: "Underwater Camouflage",
+          description: "advantage on Stealth checks while underwater",
         },
       ],
     },
@@ -626,6 +734,7 @@ const defaultCreatureList: Creature[] = [
     ],
   },
   giantToad,
+  giantOctopus,
 ];
 
 const creatureList = useStorage<Creature[]>(
